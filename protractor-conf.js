3d5364760aa3da24f0c9b40a5fe15767434ee22c
Shahar Talmi <shahar.talmi@gmail.com>
2015-05-28 15:07:05 +0300

    added options to have protractor-base-conf

--- protractor-conf.js
+++ protractor-conf.js
@@ -1,57 +1,67 @@
-/* global browser, angular */
+/* global browser, angular, document, beforeEach */
 'use strict';
 
-module.exports.config = {
-  allScriptsTimeout: 120000,
+var config = {};
+try {
+  config = require(process.cwd() + '/protractor-base-conf').config;
+} catch (e) {
 
-  baseUrl: 'http://localhost:9000/',
+}
 
-  specs: [
-    process.cwd() + '/test/spec/e2e/**/*.js',
-    process.cwd() + '/test/e2e/spec/**/*.js'
-  ],
+config.allScriptsTimeout = 120000;
 
-  framework: 'jasmine',
+config.baseUrl = 'http://localhost:9000/';
 
-  capabilities: {
-    browserName: 'chrome'
-  },
+config.specs = [
+  process.cwd() + '/test/spec/e2e/**/*.js',
+  process.cwd() + '/test/e2e/spec/**/*.js'
+];
 
-  onPrepare: function () {
-    // Disable animations so e2e tests run more quickly
-    var disableNgAnimate = function () {
-      angular.module('disableNgAnimate', []).run(function ($animate) {
-        $animate.enabled(false);
-      });
-    };
+config.framework = 'jasmine';
 
-    var disableCssAnimate = function () {
-      angular.module('disableCssAnimate', []).run(function () {
-        var style = document.createElement('style');
-        style.type = 'text/css';
-        style.innerHTML = '* {' +
-          '-webkit-transition: none !important;' +
-          '-moz-transition: none !important;' +
-          '-o-transition: none !important;' +
-          '-ms-transition: none !important;' +
-          'transition: none !important;' +
-          '}';
-        document.getElementsByTagName('head')[0].appendChild(style);
-      });
-    };
-
-    beforeEach(function () {
-        browser.addMockModule('disableNgAnimate', disableNgAnimate);
-        browser.addMockModule('disableCssAnimate', disableCssAnimate);
-    });
-
-    // Store the name of the browser that's currently being used.
-    browser.getCapabilities().then(function (caps) {
-      browser.params.browser = caps.get('browserName');
-    });
-  },
-
-  jasmineNodeOpts: {
-    defaultTimeoutInterval: 300000
-  }
+config.capabilities = {
+  browserName: 'chrome'
 };
+
+var onPrepare = config.onPrepare || function () {};
+config.onPrepare = function () {
+  // Disable animations so e2e tests run more quickly
+  var disableNgAnimate = function () {
+    angular.module('disableNgAnimate', []).run(function ($animate) {
+      $animate.enabled(false);
+    });
+  };
+
+  var disableCssAnimate = function () {
+    angular.module('disableCssAnimate', []).run(function () {
+      var style = document.createElement('style');
+      style.type = 'text/css';
+      style.innerHTML = '* {' +
+        '-webkit-transition: none !important;' +
+        '-moz-transition: none !important;' +
+        '-o-transition: none !important;' +
+        '-ms-transition: none !important;' +
+        'transition: none !important;' +
+        '}';
+      document.getElementsByTagName('head')[0].appendChild(style);
+    });
+  };
+
+  beforeEach(function () {
+      browser.addMockModule('disableNgAnimate', disableNgAnimate);
+      browser.addMockModule('disableCssAnimate', disableCssAnimate);
+  });
+
+  // Store the name of the browser that's currently being used.
+  browser.getCapabilities().then(function (caps) {
+    browser.params.browser = caps.get('browserName');
+  });
+
+  onPrepare.apply(this, arguments);
+};
+
+config.jasmineNodeOpts = {
+  defaultTimeoutInterval: 300000
+};
+
+module.exports.config = config;
