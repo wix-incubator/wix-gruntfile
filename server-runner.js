ca2913432dedd2b07fd01bb916dca941c520bc86
Shahar Talmi <shahar.talmi@gmail.com>
2015-06-28 22:59:01 +0300

    added server runner

---
+++ server-runner.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var spawn = require('child_process').spawn;
+var references = 0;
+var serverProcess;
+var promise;
+
+function clearSessionCookie() {
+  afterEach(function () {
+    browser.executeScript(function () {
+      /* global document */
+      document.cookie = 'connect.sid=';
+    });
+  });
+}
+
+function startServer(js, params) {
+  var deferred = protractor.promise.defer();
+  promise = deferred.promise;
+  serverProcess = spawn('node', [js].concat(params));
+  serverProcess.stdout.on('data', function () {
+    deferred.fulfill();
+  });
+  return deferred.promise;
+}
+
+module.exports = function (js, params) {
+  references++;
+  clearSessionCookie();
+  if (promise) {
+    browser.controlFlow().execute(function () {
+      return promise;
+    });
+  } else {
+    browser.controlFlow().execute(function () {
+      return startServer(js, params);
+    });
+  }
+  return {
+    kill: function () {
+      references--;
+      if (references === 0) {
+        serverProcess.kill();
+      }
+    }
+  };
+};
