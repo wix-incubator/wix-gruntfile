de4438d722e5d94f0be00ab40f63b1376e14591d
gil <gil@wix.com>
2015-08-12 15:37:27 +0300

    Adding local.wix.com to the hosts file (Windows & Mac) in the prerun of the tests

---
+++ EditDNS.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "127.0.0.1 local.wix.com" >> /etc/hosts
---
+++ EditDNS_Windows.bat
@@ -0,0 +1,4 @@
+@echo off
+echo  127.0.0.1 local.wix.com > %temp%\temphosts.txt
+type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
+copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
