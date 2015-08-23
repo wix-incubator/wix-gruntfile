91084c9b582182cb413b82d1fad85f99471c193e
Shahar Talmi <shahart@wix.com>
2015-08-23 21:09:59 +0300

    better workaround

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-trap "killall sc" EXIT
+trap "killall sc sc-osx && true" EXIT
 
 node_modules/wix-gruntfile/scripts/install-dependencies.sh
 node_modules/wix-gruntfile/scripts/grunt-build.sh
