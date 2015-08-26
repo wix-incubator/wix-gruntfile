9a49e8d45026e0db91e2b3f3dd5abcc0b1151076
Shahar Talmi <shahart@wix.com>
2015-08-26 18:56:20 +0300

    disable sharding when max instance is 1

--- protractor-teamcity-conf.js
+++ protractor-teamcity-conf.js
@@ -12,6 +12,9 @@ var config = require('./protractor-conf').config;
 if (process.env.BUILD_NUMBER !== '12345') {
   var onPrepare = config.onPrepare || function () {};
   config.capabilities.maxInstances = process.env.PROTRACTOR_SHARDS || 1;
+  if (config.capabilities.maxInstances === 1) {
+    config.capabilities.shardTestFiles = false;
+  }
   config.onPrepare = function () {
     require('jasmine-reporters');
     jasmine.getEnv().addReporter(new jasmine.TeamcityReporter());
