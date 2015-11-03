1d2b214f0b11d252097ab2ac09ffbb5d63cf02ea
Shahar Talmi <shahart@wix.com>
2015-11-03 16:25:47 +0200

    idiot

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt, options) {
   var path = require('path');
 
   var packageJson = grunt.file.readJSON('package.json');
-  if (!packageJson.scripts || !packageJson.scripts.build || !packageJson.scripts.release || !packageJson.scripts.release) {
+  if (!packageJson.scripts || !packageJson.scripts.build || !packageJson.scripts.release || !packageJson.scripts.test) {
     packageJson.scripts = packageJson.scripts || {};
     packageJson.scripts.build = packageJson.scripts.build || 'node_modules/wix-gruntfile/scripts/build.sh';
     packageJson.scripts.release = packageJson.scripts.release || 'node_modules/wix-gruntfile/scripts/release.sh';
