bf6775f149d10399aba2c079b6051ba1016451ce
Mantas Miliukas <mantasm@wix.com>
2017-11-14 14:44:37 +0200

    fix(scopes): add a build step for migrating the scopes

---
+++ grunt-sections/migrate-to-scopes.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function register(grunt) {
+
+  grunt.registerTask('migrate-to-scopes', function () {
+
+    const path = require('path');
+    const { update } = require('update-scopes');
+
+    const packageJson = path.join(process.cwd(), 'package.json');
+
+    let done = this.async();
+    update(packageJson)
+      .then(() => done())
+      .catch(() => done());
+
+  });
+
+};
