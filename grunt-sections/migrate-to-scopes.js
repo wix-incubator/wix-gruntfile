0fd97426a0ffd650da875c8ef18ac3fefebf86b8
Mantas Miliukas <mantasm@wix.com>
2017-11-14 14:54:10 +0200

    fix(improve): by using a safeguard

--- grunt-sections/migrate-to-scopes.js
+++ grunt-sections/migrate-to-scopes.js
@@ -2,18 +2,19 @@
 
 module.exports = function register(grunt) {
 
-  grunt.registerTask('migrate-to-scopes', function () {
+  grunt.registerTask('migrate-to-scopes',
+    process.env.MIGRATE_TO_SCOPED_PACKAGES === 'true' ? migrateToScopes : noop);
 
+  function noop() {}
+
+  function migrateToScopes() {
     const path = require('path');
     const { update } = require('update-scopes');
 
     const packageJson = path.join(process.cwd(), 'package.json');
 
     let done = this.async();
-    update(packageJson)
-      .then(() => done())
-      .catch(() => done());
-
-  });
+    update(packageJson).then(() => done()).catch(() => done());
+  }
 
 };
